42ea45ff272d947593e708cb0698974da0ef2745
Senshi Kudo <blue.kryptonyt@gmail.com>
2022-02-26 16:43:50 -0800

    Update lmOutputExtract.R

--- lmOutputExtract.R
+++ lmOutputExtract.R
@@ -2,8 +2,6 @@ lmOutputExtract <- function(mod){
       lmOutput <- data.frame(coefficients(summary(mod))[, c(1, 4)], confint(mod)) %>%
             dplyr::select(1, 3, 4, 2) %>% round(., 3) %>%
             `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
-            mutate(`P-value` = case_when(`P-value` < 0.001 ~ "<0.001*",
-                                         `P-value` < 0.05  ~ paste0(`P-value`,"*"),
-                                         `P-value` >= 0.05 ~ as.character(`P-value`)))
+            mutate(`P-value` = ifelse(`P-value` < 0.001, "<0.001*", ifelse(`P-value` < 0.05, paste0(`P-value`,"*"), as.character(`P-value`))))
       print(lmOutput)
 }
