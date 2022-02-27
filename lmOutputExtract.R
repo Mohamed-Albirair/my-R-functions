a47fab16fd702bf06e97dc375f1434c5a4b83f03
Senshi Kudo <blue.kryptonyt@gmail.com>
2022-02-27 13:36:20 -0800

    Adding "signifLevel" argument

--- lmOutputExtract.R
+++ lmOutputExtract.R
@@ -1,7 +1,9 @@
-lmOutputExtract <- function(mod){
+lmOutputExtract <- function(mod, signifLevel = 0.05){
       lmOutput <- data.frame(coefficients(summary(mod))[, c(1, 4)], confint(mod)) %>%
             dplyr::select(1, 3, 4, 2) %>% round(., 3) %>%
             `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
-            mutate(`P-value` = ifelse(`P-value` < 0.001, "<0.001*", ifelse(`P-value` < 0.05, paste0(`P-value`,"*"), as.character(`P-value`))))
+            mutate(`P-value` = case_when(`P-value` < 0.001        ~ "<0.001*",
+                                         `P-value` < signifLevel  ~ paste0(`P-value`,"*"),
+                                         `P-value` >= signifLevel ~ as.character(`P-value`)))
       print(lmOutput)
 }
