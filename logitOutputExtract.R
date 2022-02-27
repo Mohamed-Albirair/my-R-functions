e729a934976d80a29dd497536a0dd543644dc45b
Senshi Kudo <blue.kryptonyt@gmail.com>
2022-02-27 13:37:34 -0800

    Adding "signifLevel" argument

--- logitOutputExtract.R
+++ logitOutputExtract.R
@@ -1,11 +1,11 @@
-logitOutputExtract <- function(mod){
+logitOutputExtract <- function(mod, signifLevel = 0.05){
       logitOutput <- data.frame(exp(coefficients(summary(mod)))[, 1],
                                 coefficients(summary(mod))[, 4],
                                 exp(confint(mod))) %>%
             dplyr::select(1, 3, 4, 2) %>% round(., 3) %>%
             `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
-            mutate(`P-value` = case_when(`P-value` < 0.001 ~ "<0.001*",
-                                         `P-value` < 0.05  ~ paste0(`P-value`,"*"),
-                                         `P-value` >= 0.05 ~ as.character(`P-value`)))
+            mutate(`P-value` = case_when(`P-value` < 0.001        ~ "<0.001*",
+                                         `P-value` < signifLevel  ~ paste0(`P-value`,"*"),
+                                         `P-value` >= signifLevel ~ as.character(`P-value`)))
       print(logitOutput)
 }
