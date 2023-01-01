8c9d38eedd6a0f2ca3bb64c413dd834f6d08d4c7
Senshi Kudo <blue.kryptonyt@gmail.com>
2023-01-01 02:02:50 -0800

    Update logitMLMOutputExtract.R
    Removed unnecessary columns ordering step

--- logitMLMOutputExtract.R
+++ logitMLMOutputExtract.R
@@ -1,9 +1,9 @@
 logitMLMOutputExtract <- function(mod){
       logitMLMOutput <- data.frame(exp(coefficients(summary(mod)))[, 1],
-                                coefficients(summary(mod))[, 4],
-                                exp(lme4::fixef(mod)[2] - 1.96 * coefficients(summary(mod)))[, 2],
-                                exp(lme4::fixef(mod)[2] + 1.96 * coefficients(summary(mod)))[, 2]) %>%
-            dplyr::select(1, 3, 4, 2) %>% round(., 3) %>%
+                                   exp(lme4::fixef(mod)[2] - 1.96 * coefficients(summary(mod)))[, 2],
+                                   exp(lme4::fixef(mod)[2] + 1.96 * coefficients(summary(mod)))[, 2],
+                                   coefficients(summary(mod))[, 4]) %>%
+            round(., 3) %>%
             `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
             mutate(`P-value` = case_when(`P-value` < 0.001 ~ "<0.001*",
                                          `P-value` < 0.05 ~ paste0(`P-value`,"*"),
