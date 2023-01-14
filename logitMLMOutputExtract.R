d75a8ec71f68ac38c16b8158ddae483ea1969741
Senshi Kudo <blue.kryptonyt@gmail.com>
2023-01-14 13:04:54 -0800

    Leave P-values as numbers

--- logitMLMOutputExtract.R
+++ logitMLMOutputExtract.R
@@ -4,9 +4,9 @@ logitMLMOutputExtract <- function(mod){
                                    exp(lme4::fixef(mod)[2] + 1.96 * coefficients(summary(mod)))[, 2],
                                    coefficients(summary(mod))[, 4]) %>%
             round(., 3) %>%
-            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
-            mutate(`P-value` = case_when(`P-value` < 0.001 ~ "<0.001*",
-                                         `P-value` < 0.05 ~ paste0(`P-value`,"*"),
-                                         `P-value` >= 0.05 ~ as.character(`P-value`)))
+            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) # %>%
+#            mutate(`P-value` = case_when(`P-value` < 0.001 ~ "<0.001*",
+#                                         `P-value` < 0.05 ~ paste0(`P-value`,"*"),
+#                                         `P-value` >= 0.05 ~ as.character(`P-value`)))
       # print(logitMLMOutput)
 }
