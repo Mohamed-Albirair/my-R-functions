8bbec0e57139105fdb2148bc85cfacb6323cc0b0
Senshi Kudo <blue.kryptonyt@gmail.com>
2023-01-14 13:03:35 -0800

    Leave P-values as numeric

--- lmOutputExtract.R
+++ lmOutputExtract.R
@@ -1,9 +1,9 @@
 lmOutputExtract <- function(mod, signifLevel = 0.05){
       lmOutput <- data.frame(coefficients(summary(mod))[, c(1, 4)], confint(mod)) %>%
             dplyr::select(1, 3, 4, 2) %>% round(., 3) %>%
-            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
-            mutate(`P-value` = case_when(`P-value` < 0.001        ~ "<0.001*",
-                                         `P-value` < signifLevel  ~ paste0(`P-value`,"*"),
-                                         `P-value` >= signifLevel ~ as.character(`P-value`)))
+            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) # %>%
+#            mutate(`P-value` = case_when(`P-value` < 0.001        ~ "<0.001*",
+#                                         `P-value` < signifLevel  ~ paste0(`P-value`,"*"),
+#                                         `P-value` >= signifLevel ~ as.character(`P-value`)))
 #      print(lmOutput)
 }
