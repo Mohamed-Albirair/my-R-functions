a7ed598971a0d27a31bbcce1e946d24701e06b42
Senshi Kudo <blue.kryptonyt@gmail.com>
2023-01-14 13:04:14 -0800

    Leave P-values as numbers

--- logitOutputExtract.R
+++ logitOutputExtract.R
@@ -3,9 +3,9 @@ logitOutputExtract <- function(mod, signifLevel = 0.05){
                                 coefficients(summary(mod))[, 4],
                                 exp(confint(mod))) %>%
             dplyr::select(1, 3, 4, 2) %>% round(., 3) %>%
-            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
-            mutate(`P-value` = case_when(`P-value` < 0.001        ~ "<0.001*",
-                                         `P-value` < signifLevel  ~ paste0(`P-value`,"*"),
-                                         `P-value` >= signifLevel ~ as.character(`P-value`)))
+            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) # %>%
+#            mutate(`P-value` = case_when(`P-value` < 0.001        ~ "<0.001*",
+#                                         `P-value` < signifLevel  ~ paste0(`P-value`,"*"),
+#                                         `P-value` >= signifLevel ~ as.character(`P-value`)))
       # print(logitOutput)
 }
