abd3db64282a78d48ebd3d01669639c9652acde0
Senshi Kudo <blue.kryptonyt@gmail.com>
2024-02-23 03:57:07 -0800

    update TP time conversion

--- convTransProb.R
+++ convTransProb.R
@@ -1,6 +1,7 @@
-convTransProb <- function(newToOldRatio, oldTP){
+convTransProb <- function(ratio, oldTP){
       newTP = 1 - (1 - oldTP) ^ ratio
       # newTP is the transition probability over time period b we wish to estimate
       # oldTP is the known probability over time period a 
+      # ratio is b:a (e.g., when converting from annual to monthly, ratio = 1/12)
       return(newTP)
 }
