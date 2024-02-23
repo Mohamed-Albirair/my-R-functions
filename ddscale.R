5d25b45b2fb29d26271f561363fdf85bd251caef
Senshi Kudo <blue.kryptonyt@gmail.com>
2024-02-23 04:06:12 -0800

    Delete obslete files

--- ddscale.R
+++
@@ -1,6 +0,0 @@
-ddscale <- function(control, treatment){
-      n <- length(control) 
-      control.scaled <- (control - mean(control[1:(n-1)])) / (sd(control[1:(n-1)]))
-      treatment.hyp <- (control.scaled * (sd(treatment[1:(n-1)]) / sd(control.scaled[1:(n-1)])) + (mean(treatment[1:(n-1)]) - mean(control.scaled[1:(n-1)])))
-      treatment.hyp
-}
