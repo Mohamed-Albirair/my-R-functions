d31292832f794b51ef1b2bc87d8fab04ee9e7084
Senshi Kudo <blue.kryptonyt@gmail.com>
2024-02-26 04:00:30 -0800

    update model output extraction functions

--- linearOutputExtract.R
+++ linearOutputExtract.R
@@ -1,9 +1,8 @@
 linearOutputExtract <- function(mod, signifLevel = 0.05){
-      lmOutput <- data.frame(coefficients(summary(mod))[, c(1, 4)], confint(mod)) %>%
-            dplyr::select(1, 3, 4, 2) %>% round(., 3) %>%
-            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
+      data.frame(coefficients(summary(mod))[, c(1, 4)], confint(mod)) %>% 
+            dplyr::select(1, 3, 4, 2) %>% round(., 3) %>% 
+            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>% 
             mutate(`P-value` = case_when(`P-value` < 0.001        ~ "<0.001",
                                          `P-value` < signifLevel  ~ as.character(`P-value`),
                                          `P-value` >= signifLevel ~ as.character(`P-value`)))
-#      print(lmOutput)
 }
--- nonlinearMLMOutputExtract.R
+++ nonlinearMLMOutputExtract.R
@@ -1,12 +1,11 @@
 nonlinearMLMOutputExtract <- function(mod){
-      logitMLMOutput <- data.frame(exp(coefficients(summary(mod)))[, 1],
-                                   exp(lme4::fixef(mod)[2] - 1.96 * coefficients(summary(mod)))[, 2],
-                                   exp(lme4::fixef(mod)[2] + 1.96 * coefficients(summary(mod)))[, 2],
-                                   coefficients(summary(mod))[, 4]) %>%
-            round(., 3) %>%
-            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>%
+      data.frame(exp(coefficients(summary(mod)))[, 1],
+                 exp(lme4::fixef(mod)[2] - 1.96 * coefficients(summary(mod)))[, 2],
+                 exp(lme4::fixef(mod)[2] + 1.96 * coefficients(summary(mod)))[, 2],
+                 coefficients(summary(mod))[, 4]) %>% 
+            round(., 3) %>% 
+            `colnames<-`(c("Coefficient", "LCL", "UCL", "P-value")) %>% 
             mutate(`P-value` = case_when(`P-value` < 0.001 ~ "<0.001",
                                          `P-value` < 0.05 ~ as.character(`P-value`),
                                          `P-value` >= 0.05 ~ as.character(`P-value`)))
-      # print(logitMLMOutput)
 }
