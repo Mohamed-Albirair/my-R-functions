70b186cb5767c65f2da3d9222cfbd60b597142d6
Mohamed Albirair <mta10@uw.edu>
2024-03-21 05:17:43 -0700

    select(all_of)

--- mlModOutput.R
+++ mlModOutput.R
@@ -1,12 +1,12 @@
 # A manual R function for extracting regression multi-level model
 # fixed effects coefficients:
 mlModOutput <- function(mod,
-                        expCoef = FALSE,
+                        expCoef = FALSE, # default for linear models
                         cols = c(1:2, 5:6, 9)){
       mod %>% parameters::parameters(., exponentiate = expCoef,
                                      effects = "fixed") %>%
             as.data.frame() %>% # to allow further wrangling
-            dplyr::select(cols) %>% # select desired columns in the desired order
+            dplyr::select(all_of(cols)) %>% # select desired columns in the desired order
             mutate(across(2:4, ~ ifelse(abs(.) < 0.001, scales::scientific(.), round(., 3))),
                    across(last_col(), ~ ifelse(. < 0.001, "<0.001", round(., 3)))) %>%
             magrittr::set_colnames(c("Parameter", "Coefficient", "LCL", "UCL", "P-value"))
--- modOutput.R
+++ modOutput.R
@@ -4,7 +4,7 @@ modOutput <- function(mod,
                       cols = c(1:2, 5:6, 9)){
       mod %>% parameters::parameters(., exponentiate = expCoef) %>%
             as.data.frame() %>% # to allow further wrangling
-            dplyr::select(cols) %>% # select desired columns in the desired order
+            dplyr::select(all_of(cols)) %>% # select desired columns in the desired order
             mutate(across(2:4, ~ ifelse(abs(.) < 0.001, scales::scientific(.), round(., 3))),
                    across(last_col(), ~ ifelse(. < 0.001, "<0.001", round(., 3)))) %>%
             magrittr::set_colnames(c("Parameter", "Coefficient", "LCL", "UCL", "P-value"))
