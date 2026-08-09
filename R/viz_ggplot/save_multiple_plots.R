3a412c7614987a31810a8cdc864f9cb466e87e1c
Mohamed Albirair <mta10@uw.edu>
2026-08-09 16:11:44 -0700

    Update save_multiple_plots.R

--- R/viz_ggplot/save_multiple_plots.R
+++ R/viz_ggplot/save_multiple_plots.R
@@ -3,23 +3,30 @@
 
 save_multiple_plots <- function(plot_list,
                                 file_loc,
-                                plot_name = names(plot_list),
+                                plot_name   = names(plot_list),
                                 extension   = ".png",
                                 width_inpt  = 900,
                                 height_inpt = 600,
                                 units_inpt  = "px",
-                                dpi_inpt    = 300) {
+                                dpi_inpt    = 300,
+                                verbos      = TRUE) {
       
       mapply(
-            FUN  = function(plot, name) {
+            FUN  = function(plot, name, idx) {
+                  if (verbos) {
+                        message("Saving plot ", idx, " of ", length(plot_list), ": ", name)
+                  }
+                  
                   ggsave(filename = paste0(file_loc, name, extension),
-                         plot     = plot_list,
+                         plot     = plot,
                          width    = width_inpt,
                          height   = height_inpt,
                          units    = units_inpt,
                          dpi      = dpi_inpt)
             },
-            plot = plot_list,
-            name = plot_name
+            plot     = plot_list,
+            name     = plot_name,
+            idx      = seq_along(plot_list),
+            SIMPLIFY = FALSE
       )
 }
